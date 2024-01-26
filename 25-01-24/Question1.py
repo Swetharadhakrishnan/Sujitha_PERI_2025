448ff414ec57e36faff24812bd316bf0ce0abf1d
Sujitha55 <146987992+Sujitha55@users.noreply.github.com>
2024-01-26 05:07:22 -1200

    Update Question1.py

--- 25-01-24/Question1.py
+++ 25-01-24/Question1.py
@@ -1,3 +1,54 @@
+''' Cities on a map are connected by a number of roads. The number of roads between each city is in an array and city  is the starting location. The number of roads from city  to city  is the first value in the array, from city  to city  is the second, and so on.
+
+How many paths are there from city  to the last city in the list, modulo ?
+
+Example
+
+
+There are  roads to city ,  roads to city  and  roads to city . The total number of roads is .
+
+Note
+Pass all the towns Ti for i=1 to n-1 in numerical order to reach Tn.
+
+Function Description
+
+Complete the connectingTowns function in the editor below.
+
+connectingTowns has the following parameters:
+
+int n: the number of towns
+int routes[n-1]: the number of routes between towns
+Returns
+
+int: the total number of routes, modulo 1234567.
+Input Format
+The first line contains an integer T, T test-cases follow.
+
+Each test-case has 2 lines.
+The first line contains an integer N (the number of towns).
+The second line contains N - 1 space separated integers where the ith integer denotes the number of routes, Ni, from the town Ti to Ti+1
+
+Constraints
+1 <= T<=1000
+2< N <=100
+1 <= routes[i] <=1000
+
+Sample Input
+
+2
+3
+1 3
+4
+2 2 2
+Sample Output
+
+3
+8
+Explanation
+Case 1: 1 route from T1 to T2, 3 routes from T2 to T3, hence only 3 routes.
+Case 2: There are 2 routes from each city to the next, hence 2 * 2 * 2 = 8. '''
+
+
 #!/bin/python3
 
 import math
@@ -7,38 +58,33 @@ import re
 import sys
 
 #
-# Complete the 'findPoint' function below.
+# Complete the 'connectingTowns' function below.
 #
-# The function is expected to return an INTEGER_ARRAY.
+# The function is expected to return an INTEGER.
 # The function accepts following parameters:
-#  1. INTEGER px
-#  2. INTEGER py
-#  3. INTEGER qx
-#  4. INTEGER qy
+#  1. INTEGER n
+#  2. INTEGER_ARRAY routes
 #
 
-def findPoint(px, py, qx, qy):
-    return str(2*qx-px),str(2*qy-py)
+def connectingTowns(n, routes):
+    s=1
+    for i in routes:
+        s*=i
+    return s%1234567
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input().strip())
+    t = int(input().strip())
 
-    for n_itr in range(n):
-        first_multiple_input = input().rstrip().split()
+    for t_itr in range(t):
+        n = int(input().strip())
 
-        px = int(first_multiple_input[0])
+        routes = list(map(int, input().rstrip().split()))
 
-        py = int(first_multiple_input[1])
+        result = connectingTowns(n, routes)
 
-        qx = int(first_multiple_input[2])
-
-        qy = int(first_multiple_input[3])
-
-        result = findPoint(px, py, qx, qy)
-
-        fptr.write(' '.join(map(str, result)))
-        fptr.write('\n')
+        fptr.write(str(result) + '\n')
 
     fptr.close()
+
